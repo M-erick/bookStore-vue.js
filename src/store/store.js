c0c2c5a4830023c2dba73e04643405ce1c5c093d
CYTONN <cytonn@CYTONNs-MacBook-Pro.local>
2024-04-17 16:31:04 +0300

    use vuex in store

--- src/store/store.js
+++ src/store/store.js
@@ -1,15 +1,22 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-Vue.use(Vuex);
 
- export const store = new Vuex.Store({
-    state:{
-        jobs:[
-            {title:'study vue.js',id:1,details:'engineer'},
-            {title:'study vue.js',id:2,details:'engineer'},
-            {title:'study vue.js',id:3,details:'engineer'}
+// store/store.js
+import { createStore } from 'vuex';
+
+export default createStore({
+  state: {
+    jobs:[
+        {title:'Software Engineer',id:1,details:'Reporting to the Senior Manager - Access Network Planning & Design, the role holder is responsible for the Design, plan and optimization of the wireless radio access network communications systems, including radio propagation modelling and prediction, interference analysis, frequency coordination and parameter definition.'},
+        {title:'Data Engineer',id:2,details:'Data engineers typically have a background in Data Science, Software Engineering, Math, or a business-related field. Depending on their job or industry, most data engineers get their first entry-level job after earning their bachelor'},
+        {title:'Client Service',id:3,details:'Data engineers typically have a background in Data Science, Software Engineering, Math, or a business-related field. Depending on their job or industry, most data engineers get their first entry-level job after earning their bachelors degree'},
+        {title:'Network Administrator',id:4,details:'In-depth understanding of network protocols (TCP/IP, DNS, DHCP, etc.), network topologies (LAN, WAN, etc.), and network devices (routers, switches, firewalls, etc.) is essential.'},
+        {title:'Database Administrator',id:5,details:'In-depth understanding of network protocols (TCP/IP, DNS, DHCP, etc.), network topologies (LAN, WAN, etc.), and network devices (routers, switches, firewalls, etc.) is essential.'}
 
 
-        ]
-    }
-})
+    ]
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
+    },
+  },
+});
--- src/views/jobs/JobsView.vue
+++ src/views/jobs/JobsView.vue
@@ -15,19 +15,10 @@
 
 <script>
 export default {
-    data(){
-        return{
-          //pass th
-            jobs:[
-                {title:'Software Engineer',id:1,details:'Reporting to the Senior Manager - Access Network Planning & Design, the role holder is responsible for the Design, plan and optimization of the wireless radio access network communications systems, including radio propagation modelling and prediction, interference analysis, frequency coordination and parameter definition.'},
-                {title:'Data Engineer',id:2,details:'Data engineers typically have a background in Data Science, Software Engineering, Math, or a business-related field. Depending on their job or industry, most data engineers get their first entry-level job after earning their bachelor'},
-                {title:'Client Service',id:3,details:'Data engineers typically have a background in Data Science, Software Engineering, Math, or a business-related field. Depending on their job or industry, most data engineers get their first entry-level job after earning their bachelors degree'},
-                {title:'Network Administrator',id:4,details:'In-depth understanding of network protocols (TCP/IP, DNS, DHCP, etc.), network topologies (LAN, WAN, etc.), and network devices (routers, switches, firewalls, etc.) is essential.'},
-                {title:'Database Administrator',id:5,details:'In-depth understanding of network protocols (TCP/IP, DNS, DHCP, etc.), network topologies (LAN, WAN, etc.), and network devices (routers, switches, firewalls, etc.) is essential.'}
-
-
-            ]
-        }
+    computed:{
+      jobs(){
+        return this.$store.state.jobs
+      }
     }
 
 }
